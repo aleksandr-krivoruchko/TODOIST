06c42d0ba415d2fb78a088450ad9a88161dbc0c1
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-11 21:25:47 +0300

    fixed 404

--- src/api.js
+++ src/api.js
@@ -3,71 +3,68 @@ axios.defaults.baseURL = "https://61e433a4fbee6800175eb245.mockapi.io/api/v1";
 
 // !==========используем localStorage===============
 export function saveData(key, payload) {
-	const delay = ~~ (Math.random() * 3000);
+  const delay = ~~(Math.random() * 3000);
 
-return new Promise((resolve, reject) => {
-setTimeout(() => {
-	localStorage.setItem(key, JSON.stringify(payload));
-resolve();
-	}, delay)
-})
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      localStorage.setItem(key, JSON.stringify(payload));
+      resolve();
+    }, delay);
+  });
 }
 
 export function fetchTodos(key) {
-const delay = ~~ (Math.random() * 3000);
+  const delay = ~~(Math.random() * 3000);
 
-return new Promise((resolve, reject) => {
-setTimeout(() => {
-try {
-		resolve(JSON.parse(localStorage.getItem(key)));
-	} catch (error) {
-reject("ERROR loading TODOS (((");
-	}
-	}, delay)
-})
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        resolve(JSON.parse(localStorage.getItem(key)));
+      } catch (error) {
+        reject("ERROR loading TODOS (((");
+      }
+    }, delay);
+  });
 }
-
 
 // !============используем сервер он-лайн MOCK API + fetch + axios=======
 
-export function getTodos(){
-	// return fetch("https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST").then(res => res.json())
-	return axios.get("/TODOIST").then(res => res.data);
+export function getTodos() {
+  // return fetch("https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST").then(res => res.json())
+  return axios.get("/TODOIST").then((res) => res.data);
 }
 
 export function createTodo(newTodo) {
-// 	const options = {
-// method: "POST",
-// body: JSON.stringify(newTodo),
-// headers:{
-// 	"Content-Type": "application/json"
-// }
-// 	}
-// 		return fetch("https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST", options)
-// 		.then(res => res.json())
+  // 	const options = {
+  // method: "POST",
+  // body: JSON.stringify(newTodo),
+  // headers:{
+  // 	"Content-Type": "application/json"
+  // }
+  // 	}
+  // 		return fetch("https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST", options)
+  // 		.then(res => res.json())
 
-		return axios.post("/TODOIST", newTodo)
-		.then(res => res.data)
+  return axios.post("/TODOIST", newTodo).then((res) => res.data);
 }
 
 export function updateTodo(id, payload) {
-// const options = {
-//   method: "PUT",
-//   body: JSON.stringify(payload),
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// };
+  // const options = {
+  //   method: "PUT",
+  //   body: JSON.stringify(payload),
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // };
 
-// return fetch(`https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST/${id}`, options)
-//   .then(response => response.json())
-	return axios.put(`/TODOIST/${id}`, payload).then(response => response.data);
-
+  // return fetch(`https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST/${id}`, options)
+  //   .then(response => response.json())
+  return axios.put(`/TODOIST/${id}`, payload).then((response) => response.data);
 }
 
 export function removeTodo(id) {
-// 	return fetch(`https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST/${id}`, {
-//   method: "DELETE",
-// }).then(response => response.json());
-	return axios.delete(`/TODOIST/${id}`).then(response => response.data);
+  // 	return fetch(`https://61e433a4fbee6800175eb245.mockapi.io/api/v1/TODOIST/${id}`, {
+  //   method: "DELETE",
+  // }).then(response => response.json());
+  return axios.delete(`/TODOIST/${id}`).then((response) => response.data);
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import {createTodo, getTodos, updateTodo, removeTodo, removeAllTodos} from "./api";
+import { createTodo, getTodos, updateTodo, removeTodo } from "./api";
 import toastr, { error } from "toastr";
-import * as basicLightbox from 'basiclightbox';
+import * as basicLightbox from "basiclightbox";
 
 import "toastr/build/toastr.min.css";
 import "basiclightbox/dist/basicLightbox.min.css";
-import './css/style.css';
+import "./css/style.css";
 
 toastr.options = {
-  "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": true,
-  "positionClass": "toast-top-left",
-  "preventDuplicates": false,
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "3000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: "toast-top-left",
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "3000",
+  extendedTimeOut: "1000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut",
+};
 
-const itemTemplate = ({ id, label, checked}) =>
+const itemTemplate = ({ id, label, checked }) =>
   `<li data-id=${id} class="item ${checked ? "done" : "need"}">
     <label>
-      <input type="checkbox" ${checked ? 'checked' : ''} />
+      <input type="checkbox" ${checked ? "checked" : ""} />
       <span>${label}</span>
     </label>
     <button type="button" class="close-btn ">x</button>
   </li>`;
 
-  const deleteTodo = basicLightbox.create(`
+const deleteTodo = basicLightbox.create(`
  <div class="delete-modal">
  	<h1>Do you really want to delete this TODO ???</h1>
  	<p id='text'>my todo</p>
@@ -42,147 +42,142 @@ const itemTemplate = ({ id, label, checked}) =>
  	</div>
  	`);
 
-	   const loadingModal = basicLightbox.create(`
+const loadingModal = basicLightbox.create(`
  <div class="loading-modal">
  	<p id='text'>Please wait a bit ...</p>
  	</div>
  	`);
 
+const refs = {
+  form: document.querySelector("form"),
+  list: document.querySelector(".list"),
+  clearBtn: document.querySelector(".clear-btn"),
+  modalText: deleteTodo.element().querySelector("#text"),
+  modalBtnDelete: deleteTodo.element().querySelector(".button-modal-del"),
+  modalBtnCancel: deleteTodo.element().querySelector(".button-modal-cancel"),
+};
 
-const refs = { 
-form: document.querySelector('form'),
-list: document.querySelector('.list'),
-clearBtn: document.querySelector(".clear-btn"),
-modalText: deleteTodo.element().querySelector("#text"),
-modalBtnDelete: deleteTodo.element().querySelector(".button-modal-del"),
-modalBtnCancel: deleteTodo.element().querySelector(".button-modal-cancel")
-}
-
-let todos = [
-// {id:1, label:"buy milk", checked:true},
-// {id:2, label:"buy milk", checked:false},
-// {id:3, label:"buy milk", checked:false},
-// {id:4, label:"buy milk", checked:false},
-];
+let todos = [];
 
 let currentId;
 
 function addEventListeners() {
-	refs.list.addEventListener("click", handleClick);
-refs.clearBtn.addEventListener("click", onClearBtn);
-refs.form.addEventListener("submit", onSubmitBtn);
-refs.modalBtnDelete.addEventListener("click", onModalBtnDelete);
-refs.modalBtnCancel.addEventListener("click", onModalBtnCancel);
-
+  refs.list.addEventListener("click", handleClick);
+  refs.clearBtn.addEventListener("click", onClearBtn);
+  refs.form.addEventListener("submit", onSubmitBtn);
+  refs.modalBtnDelete.addEventListener("click", onModalBtnDelete);
+  refs.modalBtnCancel.addEventListener("click", onModalBtnCancel);
 }
- 
+
 function start() {
-loadingModal.show();
-getTodos('todos')
-.then((data) => {
-	todos = data;
-	render();
-})
-.catch((errorMessage) => {
-	toastr.error(errorMessage);
-})
-.finally(() => {
-	addEventListeners();
-	loadingModal.close();
-})
+  loadingModal.show();
+  getTodos("todos")
+    .then((data) => {
+      if (data.length === 0) {
+      }
+      console.log(data);
+      todos = data;
+      render();
+    })
+    .catch(() => {
+      toastr.error("You have no saved entries");
+    })
+    .finally(() => {
+      addEventListeners();
+      loadingModal.close();
+    });
 }
 
 function render() {
-	const items = todos.map(todo => itemTemplate(todo));
-refs.list.innerHTML = items.join('');
+  const items = todos.map((todo) => itemTemplate(todo));
+  refs.list.innerHTML = items.join("");
 }
 
 function addTodo(value) {
-		const newTodo = {label:value, checked:false};
+  const newTodo = { label: value, checked: false };
 
-toastr.success("Your TODO is created successfully)))");
+  toastr.success("Your TODO is created successfully)))");
 
-	return createTodo(newTodo).then(data => {
-		todos.push(data);
-	})
+  return createTodo(newTodo).then((data) => {
+    todos.push(data);
+  });
 }
 
 function onSubmitBtn(e) {
-	e.preventDefault();
-	const value = e.currentTarget.elements.input.value;
+  e.preventDefault();
+  const value = e.currentTarget.elements.input.value;
 
-	if(!value) return toastr.success("Enter details about your plans");
+  if (!value) return toastr.success("Enter details about your plans");
 
-addTodo(value)
-.then(refs.form.reset())
-.then(render);
+  addTodo(value).then(refs.form.reset()).then(render);
 }
 
 function onClearBtn(e) {
- Promise.all(todos.map((todo)=> removeTodo(todo.id)))
-.then(data => {
-	todos = data;
-	refs.list.innerHTML = "";
-	toastr.success("Your todos are deleted!")
-})	
+  Promise.all(todos.map((todo) => removeTodo(todo.id))).then((data) => {
+    todos = data;
+    refs.list.innerHTML = "";
+    toastr.success("Your todos are deleted!");
+  });
 }
 
-function handleClick(e){
-	const id = e.target.closest('li').dataset.id; 
+function handleClick(e) {
+  const id = e.target.closest("li").dataset.id;
 
-	switch (e.target.nodeName) {
-		case 'BUTTON':
-			deleteItem(id);
-			break;
+  switch (e.target.nodeName) {
+    case "BUTTON":
+      deleteItem(id);
+      break;
 
-		case 'INPUT':
-		case 'LABEL':
-		case 'SPAN':
-			toggleItem(id);
-			break;
+    case "INPUT":
+    case "LABEL":
+    case "SPAN":
+      toggleItem(id);
+      break;
 
-		default:
-			break;
-	}
-	render();
+    default:
+      break;
+  }
+  render();
 }
 
 function deleteItem(id) {
-	      currentId = id;
-			const {label} = todos.find(todo => todo.id == id);
-		refs.modalText.textContent = label;
-		deleteTodo.show();
-
+  currentId = id;
+  const { label } = todos.find((todo) => todo.id == id);
+  refs.modalText.textContent = label;
+  deleteTodo.show();
 }
 
 function toggleItem(id) {
-		const todo = todos.find((todo) => todo.id === id);
-		const payload = {
-			...todo,
-			checked: !todo.checked,
-		}
-		updateTodo(id, payload)
-		.then((data)=> {
-			todos = todos.map((todo)=> todo.id === id ? data : todo)
-		}).finally(()=> {
-			render();
-		})
-
+  const todo = todos.find((todo) => todo.id === id);
+  const payload = {
+    ...todo,
+    checked: !todo.checked,
+  };
+  updateTodo(id, payload)
+    .then((data) => {
+      todos = todos.map((todo) => (todo.id === id ? data : todo));
+    })
+    .finally(() => {
+      render();
+    });
 }
 
 function onModalBtnCancel(e) {
-	deleteTodo.close();
+  deleteTodo.close();
 }
 
 function onModalBtnDelete(e) {
-	removeTodo(currentId)
-	.then(() => {toastr.warning("Your TODO is deleted now!!!")})
-	.then(()=>{todos = todos.filter(todo => todo.id != currentId)})
-	.finally(()=>{	
-		render();
-		deleteTodo.close();
-})
+  removeTodo(currentId)
+    .then(() => {
+      toastr.warning("Your TODO is deleted now!!!");
+    })
+    .then(() => {
+      todos = todos.filter((todo) => todo.id != currentId);
+    })
+    .finally(() => {
+      render();
+      deleteTodo.close();
+    });
 }
 
 start();
-
